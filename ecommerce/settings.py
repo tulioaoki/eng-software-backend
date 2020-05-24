419ceed742bc77d9de3efb741da5609dfe3d9b74
Tulio Aoki <tulioaoki@gmail.com>
2020-05-24 02:20:12 -0300

    sqlite

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -82,16 +82,15 @@ AUTH_USER_MODEL = 'accounts.CustomUser'
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-"""
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 """
-
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
@@ -102,6 +101,7 @@ DATABASES = {
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
+"""
 
 
 # Password validation
